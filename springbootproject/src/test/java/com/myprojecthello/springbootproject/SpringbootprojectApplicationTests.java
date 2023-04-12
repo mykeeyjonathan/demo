c462fb9f848225523e30547679745053dc5e5071
mykeeyjonathan <130032816+mykeeyjonathan@users.noreply.github.com>
2023-04-12 13:48:53 +0300

    Initial commit

---
+++ springbootproject/src/test/java/com/myprojecthello/springbootproject/SpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecthello.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
